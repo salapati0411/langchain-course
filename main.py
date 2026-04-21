1fe06975b5d9f21076456c89d5503a1c5b05bf44
Sudhakar Alapati <sudhakarklc@gmail.com>
2026-04-21 15:45:36 -0500

    structured output changes

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from dotenv import load_dotenv
+from typing import List
+from pydantic import BaseModel, Field
+from regex import search
+
+
 load_dotenv()
 
 from langchain.agents import create_agent
@@ -7,13 +12,23 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """ Schema for a source used by the agent. """
+    url: str = Field(description="The url of the source")
+
+class AgentResponse(BaseModel):
+    """ Schema for the result returned by the agent with answer and sources. """
+    answer: str = Field(description="The answer to the question")
+    sources: List[Source] = Field(default_factory=list, description="The sources used to answer the question")
+    
+
 search = TavilySearch()
 
 
 llm= ChatOpenAI()
 tools = [search]
-agent = create_agent(model=llm, tools=tools)
-
+# agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
