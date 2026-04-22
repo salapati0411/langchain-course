50139dbc7d5d24617303e8309c770fb86acb1a0d
Sudhakar Alapati <sudhakarklc@gmail.com>
2026-04-22 16:01:18 -0500

    save initial changes

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
